4178d4b31a17ce726b526d1ee8782ba99fd2b616
Lenovo-PC\Owner <acloet@unomaha.edu>
2018-02-27 14:21:39 -0600

    Initial commit

---
+++ app/src/main/java/com/acloetlive/creativewriting/MainActivity.java
@@ -0,0 +1,24 @@
+package com.acloetlive.creativewriting;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        configureTipsButton();
+    }
+       private void configureTipsButton(){
+        Button tipsButton = (Button) findViewById(R.id.tipsButton);
+
+    }
+    }
+
---
+++ app/src/main/java/com/acloetlive/creativewriting/mystery.java
@@ -0,0 +1,13 @@
+package com.acloetlive.creativewriting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mystery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mystery);
+    }
+}
